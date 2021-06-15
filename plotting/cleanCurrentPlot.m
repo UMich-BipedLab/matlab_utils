0e8f39b1514c86754392f8f2e33276f377978379
Brucebot <bjhuang@umich.edu>
2021-06-15 19:58:49 -0400

    handled if no_show doesn't exist

--- plotting/cleanCurrentPlot.m
+++ plotting/cleanCurrentPlot.m
@@ -1,4 +1,7 @@
 function figure_handle = cleanCurrentPlot(fig, show)
+    if ~exist('show', 'var') || isempty(show)
+        show = 0;
+    end
     [~, figure_handle] = checkFigureHandle(fig, 1, 1);
     if show
         viewCurrentPlot(figure_handle)
