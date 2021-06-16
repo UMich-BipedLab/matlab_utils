d2e78f716f7f3f69bf2075b9701aba5eccbafbd0
Brucebot <bjhuang@umich.edu>
2021-06-15 20:00:16 -0400

    formatting

--- plotting/getCurrentFigure.m
+++ plotting/getCurrentFigure.m
@@ -1,5 +1,4 @@
-function [axes_h, fig_h] = ...
-    getCurrentFigure(cur_num, axes_handles, fig_handles)
+function [axes_h, fig_h] = getCurrentFigure(cur_num, axes_handles, fig_handles)
     axes_h = axes_handles(cur_num);
     fig_h = fig_handles(cur_num);
 end
